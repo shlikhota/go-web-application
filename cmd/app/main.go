de18e5b5049e60fa859801b1034d4cf8fb1a5790
Evgenii Shlikhota <shlikhota@gmail.com>
2024-01-08 21:17:03 +0100

    check heroku api key

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -17,7 +17,16 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/health", h.Health)
 
-	addr := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+	if port == "" {
+		port = "8080"
+	}
+	addr := fmt.Sprintf("%s:%s", host, port)
+	fmt.Println("Starting server", addr, "...")
 	err := http.ListenAndServe(addr, r)
 	log.Fatal(err)
 }
